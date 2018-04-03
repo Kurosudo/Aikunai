b5c3b7d1ea04167eb12dc4c5fcc5a7fc5d41c3f1
Kurosudo <akimogesk@gmail.com>
2018-04-03 23:44:56 +0200

    New design

--- index.php
+++ index.php
@@ -1,22 +1,51 @@
-<?php
-include("variables.php");
-
-if ($file_dir == "") {
-
-	echo "Please setup your file_dir in variables.php";
-	die();
-} 
-?>
+<?php  include('variables.php'); ?>
 <html>
-	<head>
-		<title>Aikunai - Anime KODI renaming tool</title>
-		<meta charset="utf-8">
-	</head>
+<head>
+<title>Aikunai</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<style>
+body{
+background-color: #212121;
+color: white;
+}
+.main{
+margin-left: 2cm;
+margin-right: 2cm;
+margin-top: 1cm;
+background-color: #121212;
+top: 1cm;
+width: 900px;
+    margin: auto;
+    border: 1px solid #000;
+}
+.content{
+padding: 0.2cm;
+}
+#s2controllers{
+  display: none;
+  }
+
+</style>
+</head>
 <body>
-<center><h1>Aikunai</h1><p>Renaming tool for KODI</p>
-<p>Current working with directory: <b><?php echo $file_dir; ?></b> </p> 
-<form action="action.php">
-<select name="dir">
+<br/>
+<div class="main">
+<div class="content">
+
+  <div class="row">
+    <div class="col">
+      <h1>Aikunai</h1><p>Renaming tool for anime</p>
+    </div>
+    <div class="col">
+	<form action="action.php">
+      <p class="text-center" style="margin-top: 0.8cm;"><input class="btn btn-success" type="submit"></p>
+    </div>
+  </div>
+
+<div class="form-row">
+    <div class="col">
+	<p>Anime</p>
+	<select class="form-control" name="dir">
 <?php
 
 $dir = scandir($file_dir);
@@ -30,12 +59,26 @@ echo "<option value='".$file."'>" . $file . "</option>";
 }
 
 ?>
-</select>
-<br/><p>Write Season, if it is first season leave it empty. Please use syntax example 03</p>
-<input type="text" name="season">
-<br/><p>If you already have first or another seasons please select location of it to merge seasons. Again if it is first season leave it empty</p>
-<br/><select name='movefolder'>
+	</select>
+    </div>
+    <div class="col">
+	<p>Season</p>
+      <input id="season" type="text" value="" class="form-control" name="season" placeholder="exp.02 Leave it empty if it is first season " autocomplete="off" >
+    </div>
+    <div class="col">
+	<p>Video container</p>
+	<select name="container"class="form-control">
+		<option value="mkv">MKV</option>
+		<option value="avi">AVI</option>
+		<option value="mp4">MP4</option>
+	</select>
+   </div>
+</div>
+<br/><div id="s2controllers">
+<p style="text-center">Folder of first season. If you want create new folder leave it empty</center>
+<select name="movefolder" class="form-control">
 <option value='nothing' selected></option>
+
 <?php
 
 $dir = scandir($file_dir);
@@ -49,13 +92,20 @@ echo "<option value='".$file_dir."/".$file."'>" . $file . "</option>";
 }
 
 ?>
+
+
 </select>
-<br/><p>Video container of file (example mkv)</p>
-<input type="text" name="container" required>
-<br/><br/><input type="submit">
+</div>
+
 </form>
-</center>
-
-
+<br/>
+<p class="text-center">Copyright &copy 2018 Kurosudo <br/><a target="_blank" href="https://raw.githubusercontent.com/Kurosudo/Aikunai/master/LICENSE">LICENSE</a>|<a href="https://github.com/Kurosudo/Aikunai">GitHub Repo</a></p>
+</div>
+</div>
 </body>
-</html>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+$("#season").keyup(function(){
+        $("#s2controllers").show();
+});
+</script>
