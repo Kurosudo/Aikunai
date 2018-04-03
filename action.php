d7505be2240d551d20b2ee6e37780aab981466da
Kurosudo <akimogesk@gmail.com>
2018-04-03 23:44:32 +0200

    Fixed bugs, improved mechanism and some other things

--- action.php
+++ action.php
@@ -20,10 +20,23 @@ foreach($sdir as $file) {
 
 if (strpos($file, '.mkv') !== false) {
 	$epnum = exec("echo '".$file."' | sed -e 's/\[[^][]*\]//g' | grep -o '[0-9]\+'");
+	echo $epnum;
+	
+	if (strpos($epnum, '480') !== false || strpos($epnum, '720') !== false || strpos($epnum, '1080') !== false) {
+    	echo 'founded bug, fixing';
+
+		$epnum = exec("echo '".$file."' | sed -e 's/\[[^][]*\]//g' | grep -o '[0-9]\+' | head -n -1");	
+	}
+
+	echo $epnum;
 	$finalname = "S".$season."E".$epnum.".".$container."";
+	echo $finalname;
+	echo "<br>";
+	echo "".$dir."/".$file."";
 	exec("mv '".$dir."/".$file."' '".$dir."/".$finalname."'");
 	if ($season == "01") {
-		$directoryfix = exec("echo '".$odir."' | sed -e 's/\[[^][]*\]//g' ");
+		$directoryfix = exec("echo '".$odir."' | sed -e 's/\[[^][]*\]//g' | sed 's/^.//' ");
+		
 		exec("mv '".$dir."' '".$file_dir."/".$directoryfix."'");
 	}
 	if ($movefolder != "nothing") {
